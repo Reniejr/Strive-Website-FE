2d0add47e69f23828f27c9b150563ffdb15eb289
RenieJr <reniejr@gmail.com>
2021-04-13 15:44:56 +0200

    updates

---
+++ src/Components/_Pages/7.TeacherPage/StudentList/StudentCard/StudentCard.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from "react";
+
+//STYLE IMPORTS
+import "./StudentCard.scss";
+
+export default function StudentCard({ state }) {
+  const [lastExam, setLastExam] = useState(null);
+  const [lastHw, setLastHw] = useState(null);
+  const [lastLesson, setLastLesson] = useState(null);
+  const [student, setStudent] = useState(null);
+
+  useEffect(() => {
+    setStudent(state.student);
+    console.log(state.student);
+    setLastExam(
+      state.student.studentInfo.exams[
+        state.student.studentInfo.exams.length - 1
+      ]
+    );
+    setLastHw(
+      state.student.studentInfo.homeworks[
+        state.student.studentInfo.homeworks.length - 1
+      ]
+    );
+    setLastLesson(
+      state.student.studentInfo.attendance[
+        state.student.studentInfo.attendance.length - 1
+      ]
+    );
+  }, [state.student]);
+
+  return (
+    <div className="student-card-teacher">
+      {student !== null ? (
+        <>
+          <div className="basic-info">
+            <img src={student.profile} alt="" />
+            <div className="infos">
+              <p>
+                {student.firstName} {student.lastName}
+              </p>
+              <p>
+                Github :{" "}
+                <a
+                  href={`https://github.com/${student.githubUsername}`}
+                  target="_blank"
+                >
+                  https://github.com/{student.githubUsername}
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="status">
+            <p>Last Benchmark</p>
+            <table>
+              <tr>
+                <th>Benchmark Name</th>
+                <th>Benchmark Type</th>
+                <th>Results</th>
+              </tr>
+              <tr>
+                <td>{lastExam.examName}</td>
+                <td>{lastExam.examType}</td>
+                <td>{(lastExam.points * 100) / lastExam.quests.length}%</td>
+              </tr>
+            </table>
+            <p>Last Lesson</p>
+            <table>
+              <tr>
+                <th>Module - Day</th>
+                <th>Topic</th>
+                <th>Present</th>
+              </tr>
+              <tr>
+                <td>
+                  {lastLesson.module} - {lastLesson.day}
+                </td>
+                <td>{lastLesson.topic}</td>
+                <td>
+                  <div
+                    className="presence-span"
+                    style={{
+                      backgroundColor: lastLesson.present ? "#00ff84" : "red",
+                    }}
+                  ></div>
+                </td>
+              </tr>
+            </table>
+            <p>Last Homework</p>
+            <table>
+              <tr>
+                <th>Module - Day</th>
+                <th>Task</th>
+                <th>Completed</th>
+              </tr>
+              <tr>
+                <td>
+                  {lastHw.module} - {lastHw.day}
+                </td>
+                <td>{lastHw.githubLink ? lastHw.githubLink : "No Repo"}</td>
+                <td>
+                  <div
+                    className="hw-completed"
+                    style={{
+                      backgroundColor: lastHw.complete ? "#00ff84" : "red",
+                    }}
+                  ></div>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+}
--- src/Components/_Pages/7.TeacherPage/StudentList/StudentList.jsx
+++ src/Components/_Pages/7.TeacherPage/StudentList/StudentList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //REDUX IMPORTS
 import { useSelector } from "react-redux";
@@ -8,9 +8,12 @@ import { Container, Row, Col } from "react-bootstrap";
 
 //STYLE IMPORT
 import "./StudentList.scss";
+import StudentCard from "./StudentCard/StudentCard";
 
 export default function StudentList({ state }) {
   const batchInfo = useSelector((state) => state.batchReducer.batch);
+
+  const [student, setStudent] = useState(null);
 
   return (
     <div
@@ -18,26 +21,45 @@ export default function StudentList({ state }) {
       className="teacher-page"
       style={state.style(state.page === "students-list" ? true : false)}
     >
-      <h1>Student List of {batchInfo.name}</h1>
-      <Container>
-        <Row>
-          <Col xs={12} md={4}>
-            <ul>
-              {batchInfo.studentList.length > 0 ? (
-                batchInfo.studentList.map((s, sI) => {
-                  return (
-                    <li key={sI}>
-                      {s.firstName} {s.lastName}
-                    </li>
-                  );
-                })
-              ) : (
-                <p>No students found</p>
-              )}
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+      {batchInfo !== null ? (
+        <>
+          <h1>Student List of {batchInfo.name}</h1>
+          <Container>
+            <Row>
+              <Col xs={12} md={4}>
+                <ul>
+                  <li>
+                    Total Students : <span>{batchInfo.studentList.length}</span>
+                  </li>
+                  {batchInfo.studentList.length > 0 ? (
+                    batchInfo.studentList.map((s, sI) => {
+                      return (
+                        <li key={sI} onClick={() => setStudent(s)}>
+                          {sI + 1}.{" "}
+                          <span>
+                            {s.firstName} {s.lastName}
+                          </span>
+                        </li>
+                      );
+                    })
+                  ) : (
+                    <p>No students found</p>
+                  )}
+                </ul>
+              </Col>
+              <Col xs={12} md={8}>
+                {student !== null ? (
+                  <StudentCard state={{ student }} />
+                ) : (
+                  <p className="no-result">No Student Selected</p>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </>
+      ) : (
+        <h1>No Batch Selected</h1>
+      )}
     </div>
   );
 }
